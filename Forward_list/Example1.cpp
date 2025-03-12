a5f087a23b5e7467dbceb9c2af9a4b6c76f23177
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-03-12 17:41:58 +0530

    added

--- Forward_list/Example1.cpp
+++ Forward_list/Example1.cpp
@@ -1,25 +1,54 @@
 // Singlee linked list is called as Forward List and Doublee linked list is called as List.
 // In directed graph we use Forward list. single or uni direction
 
-#include<iostream>
-#include<forward_list>
+#include <iostream>
 using namespace std;
-int main(){
-
-  forward_list<int>f;
-  f.push_front(100);
-  f.push_front(80);
-  f.push_front(60);
-  f.push_front(40);
-  f.push_front(20);
-   for(auto p:f){
-    cout<<p<<"\t";
-   }
-   f.pop_front();
-   cout<<endl;
-   for (auto p : f)
-   {
-       cout << p << "\t";
-   }
-   cout<<"\n"<<distance(f.begin(),f.end());
+#include <forward_list>
+int main()
+{
+  forward_list<int> f;
+  f.push_front(12);
+  f.push_front(45);
+  f.push_front(56);
+  f.push_front(45);
+  f.push_front(78);
+  forward_list<int> f2;
+  f2.push_front(89);
+  f2.push_front(4);
+  f2.push_front(9);
+  f2.push_front(19);
+  f2.push_front(69);
+  for (auto p : f)
+  {
+    cout << p << "\t";
+  }
+  f.pop_front();
+  cout << endl;
+  for (auto p : f)
+  {
+    cout << p << "\t";
+  }
+  cout << endl;
+  cout << "Size =" << distance(f.begin(), f.end());
+  cout << "\n";
+  f.reverse();
+  for (auto p : f)
+  {
+    cout << p << "\t";
+  }
+  f.sort();
+  f.unique();
+  cout << "\n Unique value\n";
+  for (auto p : f)
+  {
+    cout << p << "\t";
+  }
+  cout << "\n";
+  f.sort();
+  f2.sort();
+  f.merge(f2);
+  for (auto p : f)
+  {
+    cout << p << "\t";
+  }
 }
