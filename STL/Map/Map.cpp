2f3cc85ffa22ae70154165694b4ca0749599215e
nikhil0441 <nikhilpatel03022004@gmail.com>
2025-03-20 17:50:49 +0530

    added

---
+++ STL/Map/Map.cpp
@@ -0,0 +1,16 @@
+//Map:
+//1.It is an associate(key and value) container
+//2.It contains key and value
+//3.It is sorted by default
+//4.key is always unique
+//5.Self balance binary serach tree or Red Black Tree maintain the lexicography order
+//6.There are 3 types of map
+    //(i) ordered map(sorted)
+    //(ii)unordered map(unsorted)
+    //(iii) multimap(duplicate values allowed)
+
+
+
+#include<iostream>
+#include<map>
+using namespace std;
